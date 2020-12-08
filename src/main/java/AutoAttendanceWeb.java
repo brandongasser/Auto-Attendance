995c0f32871b58cb51e47d4b53591e83f2a12a1a
brandongasser <bgasser12@gmail.com>
2020-12-08 13:48:08 -0600

    Added Specific Methods

--- src/main/java/AutoAttendanceWeb.java
+++ src/main/java/AutoAttendanceWeb.java
@@ -15,19 +15,23 @@ public class AutoAttendanceWeb {
 
 	private static final String CLASSROOM_URL = "https://classroom.google.com/w/MTkwNzg3MzA1NTE3/t/all";
 	private static final String EMAIL_URL = "https://mail.google.com/mail/u/0/#inbox";
+	private static final String FORMS_URL = "https://docs.google.com/forms/d/e/1FAIpQLScsuyMMv0HruPu3HjvG5p9v_XTGPCBIxCjg2Y1UmB5extnRxA/viewform";
+	private static final String ACCOUNTS_URL = "https://accounts.google.com/signin/v2/identifier?passive=1209600&continue=https%3A%2F%2Faccounts.google.com%2F&followup=https%3A%2F%2Faccounts.google.com%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
 
 	private static String EMAIL;
 	private static String PASSWORD;
 	private static String FIRST_NAME;
 	private static String LAST_NAME;
+	private static String GRADE;
+	private static boolean attendanceToday;
 
 	private static WebDriver driver;
 
 	public static void main(String[] args) throws Exception {
 
 		ChromeOptions options = new ChromeOptions();
-		options.setHeadless(true);
-		options.addArguments("--disable-gpu");
+		// options.setHeadless(true);
+		// options.addArguments("--disable-gpu");
 
 		driver = new ChromeDriver(options);
 
@@ -35,44 +39,29 @@ public class AutoAttendanceWeb {
 		PASSWORD = args[1];
 		FIRST_NAME = args[2];
 		LAST_NAME = args[3];
+		GRADE = args[4];
 
 		runCode();
-		// runEmailTest();
 
-	}
-
-	private static void runEmailTest() throws Exception {
-		System.out.println("Email test starting...");
-		driver.navigate().to(EMAIL_URL);
-		WebElement email = new WebDriverWait(driver, 15)
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='email']")));
-		email.sendKeys(EMAIL);
-		email.sendKeys(Keys.RETURN);
-		WebElement password = new WebDriverWait(driver, 15)
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='password']")));
-		password.sendKeys(PASSWORD);
-		password.sendKeys(Keys.RETURN);
-		new WebDriverWait(driver, 15)
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[text()='Compose']"))).click();
-		new WebDriverWait(driver, 15)
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("/descendant::textarea[1]"))).sendKeys(EMAIL);
-		new WebDriverWait(driver, 15)
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='subjectbox']")))
-				.sendKeys("Auto Attendance Confirmation "
-						+ new SimpleDateFormat("MMMM dd").format(Calendar.getInstance().getTime()));
-		new WebDriverWait(driver, 15)
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@aria-label='Message Body']")))
-				.sendKeys("Auto Attendance finished successfully at "
-						+ new SimpleDateFormat("hh:mm:ss MMMM dd").format(Calendar.getInstance().getTime()));
-		new WebDriverWait(driver, 15).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[text()='Send']")))
-				.click();
-		Thread.sleep(15000);
-		driver.close();
 	}
 
 	private static void runCode() throws Exception {
-		System.out.println("Process Starting...");
-		driver.navigate().to(CLASSROOM_URL);
+		System.out.println("AutoAttendance Starting...");
+		logIn();
+		checkForAttendance();
+		if (attendanceToday) {
+			System.out.println("Found Attendance Today");
+			doForm();
+			turnInAssignment();
+			sendEmail();
+			closeAllTabs();
+		} else {
+			System.out.println("No Attendance Today");
+		}
+	}
+
+	private static void logIn() {
+		driver.navigate().to(ACCOUNTS_URL);
 		WebElement email = new WebDriverWait(driver, 15)
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='email']")));
 		email.sendKeys(EMAIL);
@@ -81,38 +70,58 @@ public class AutoAttendanceWeb {
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='password']")));
 		password.sendKeys(PASSWORD);
 		password.sendKeys(Keys.RETURN);
+	}
 
+	private static void checkForAttendance() {
+		driver.navigate().to(CLASSROOM_URL);
 		try {
 			new WebDriverWait(driver, 15)
 					.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(), '"
-							+ new SimpleDateFormat("MMMM dd").format(Calendar.getInstance().getTime()) + "')]")))
+							+ new SimpleDateFormat("MMMM dd").format(Calendar.getInstance().getTime()) + "')]")));
+					attendanceToday = true;
+		} catch (Exception e) {
+			try {
+				new WebDriverWait(driver, 15)
+					.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(), '"
+							+ new SimpleDateFormat("MMMM d").format(Calendar.getInstance().getTime()) + "')]")));
+					attendanceToday = true;
+			} catch (Exception f) {
+				attendanceToday = false;
+			}
+		}
+	}
+
+	private static void turnInAssignment() {
+		driver.navigate().to(CLASSROOM_URL);
+		try {
+			new WebDriverWait(driver, 15)
+					.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(), '"
+							+ new SimpleDateFormat("EEEE, MMMM dd").format(Calendar.getInstance().getTime()) + "')]")))
 					.click();
 		} catch (Exception e) {
 			new WebDriverWait(driver, 15)
 					.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(), '"
-							+ new SimpleDateFormat("MMMM d").format(Calendar.getInstance().getTime()) + "')]")))
+							+ new SimpleDateFormat("EEEE, MMMM d").format(Calendar.getInstance().getTime()) + "')]")))
 					.click();
 		}
 		new WebDriverWait(driver, 15)
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='View assignment']"))).click();
-		try {
-			new WebDriverWait(driver, 15).until(
-					ExpectedConditions.elementToBeClickable(By.xpath("/descendant::div[text()='STMA ATTENDANCE'][3]")))
-					.click();
-		} catch (Exception e) {
-			new WebDriverWait(driver, 15).until(
-					ExpectedConditions.elementToBeClickable(By.xpath("/descendant::div[text()='STMA ATTENDANCE'][3]")))
-					.click();
-		}
-		ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-		driver.switchTo().window(tabs.get(1));
+				new WebDriverWait(driver, 15)
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Mark as done']"))).click();
+		new WebDriverWait(driver, 15)
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("/descendant::span[text()='Mark as done'][4]")))
+				.click();
+	}
+
+	private static void doForm() {
+		driver.navigate().to(FORMS_URL);
 		new WebDriverWait(driver, 15)
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("/descendant::input[@type='text'][1]")))
 				.sendKeys(FIRST_NAME);
 		new WebDriverWait(driver, 15)
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//descendant::input[@type='text'][2]")))
 				.sendKeys(LAST_NAME);
-		new WebDriverWait(driver, 15).until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='12']")))
+		new WebDriverWait(driver, 15).until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='" + GRADE + "']")))
 				.click();
 		new WebDriverWait(driver, 15).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='date']")))
 				.sendKeys(new SimpleDateFormat("MM/dd/YYYY").format(Calendar.getInstance().getTime()));
@@ -120,14 +129,9 @@ public class AutoAttendanceWeb {
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Present']"))).click();
 		new WebDriverWait(driver, 15)
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Submit']"))).click();
-		driver.switchTo().window(tabs.get(0));
-		new WebDriverWait(driver, 15)
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Mark as done']"))).click();
-		new WebDriverWait(driver, 15)
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("/descendant::span[text()='Mark as done'][4]")))
-				.click();
+	}
 
-		// confirmation email
+	private static void sendEmail() throws Exception {
 		driver.navigate().to(EMAIL_URL);
 		new WebDriverWait(driver, 15)
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[text()='Compose']"))).click();
@@ -144,10 +148,14 @@ public class AutoAttendanceWeb {
 		new WebDriverWait(driver, 15).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[text()='Send']")))
 				.click();
 		Thread.sleep(15000);
+	}
 
+	private static void closeAllTabs() {
+		ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
 		for (String tab : tabs) {
 			driver.switchTo().window(tab);
 			driver.close();
 		}
 	}
+
 }
